02e9350d77afa98d39bccf35f67e636d12857653
Donald Evans <devans08123@gmail.com>
2024-09-11 21:11:06 -0400

    adding updates

--- mercurySedan.js
+++ mercurySedan.js
@@ -2,34 +2,52 @@
 const VehicleModule = require("./vehicleBaseClass")
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
+let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
+class car extends vehicle{
+    constructor(make, model, year, color, mileage, maxPassengers, numberOfWheels, maximumSpeed, fuel, scheduleService){
+        super(make,model,year,color,mileage)
+        this.maxPassengers = maxPassengers
+        this.passengers = passengers
+        this.numberOfWheels= numberOfWheels
+        this.maximumSpeed= maximumSpeed
+        this.fuel= fuel
+        this.scheduleService= scheduleService
+        this.maintenance = false
+    }
+    LoadPassenger(num){
+        if(this.passengers + num<= this.maxPassengers){
+        this.passengers = this.passengers + num
+        }else{
+            console.log("that many people dont fit")
+        }
+    }
 
-//After you write the derived Car class, you should test it out.
-
-//Note: You can code your derived Car class here or make a file named index.js and do it there.
-
-
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
-
-
-
-
-
-
-
-
-
-
-
-
-//TO DO: Creating Instances and Testing Them
-
-//You can use the same instance "v" of the Vehicle class above for the base class.
+    Start(){
+        if (this.fuel >0){
+            this.start = true
+        }
+        else{
+            console.log('not enough fuel to start')
+        }
+    }
+    
+    scheduleService(mileage){
+        if(mileage > 30000){
+            this.maintenance = true
+        }
+        else{
+            console.log('car not in need of maintenance yet')
+        }
+    }
+}
 
 
+const a28 = new car('mercury','a28',1992, 'grey', 30000 , 5, 4, 160, 10, false)
 
+console.log(a28)
 
-
-//Create at least two new instances of the Car class and test them here:
+a28.LoadPassenger(2)
+console.log(a28.passengers)
+a28.LoadPassenger(4)
